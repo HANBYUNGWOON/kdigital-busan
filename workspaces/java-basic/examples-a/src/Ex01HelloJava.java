30858c7d41744cb34205df0701c513e36a5aa341
shared.repo.z <shared.repo.z@gmail.com>
2021-07-19 21:11:00 +0900

    add document and java project

---
+++ workspaces/java-basic/examples-a/src/Ex01HelloJava.java
@@ -0,0 +1,35 @@
+
+public class Ex01HelloJava {
+
+	public static void main(String[] args) { // Entry point
+		
+		System.out.println("Hello, Java with Eclipse");
+		System.out.println("Bye ~~~");
+
+	} // main 종료 : 프로그램 종료
+
+}
+
+//1. 자바 프로그램의 모든 구성요소는 클래스 내부에 만들어집니다.
+//   ( 일반적으로 하나의 파일에 하나의 클래스를 만듭니다. / 클래스 이름과 파일 이름 일치 )
+
+//2. 자바 프로그램은 public static void main(String[] args)에서 시작
+//   main이 종료되면 프로그램도 종료
+
+//3. 명령은 ;으로 끝나야 합니다.
+
+//4. 대소문자 구분
+
+/*
+5. (일반적인) 이름 규칙
+   클래스 이름은 : 	Name, UserName, HelloJava
+   변수,메서드, ... : 	name, userName, helloJava
+*/
+
+//6. 주석 : 컴파일러가 컴파일하지 않는 영역 (무시되는 명령), 코드에 대한 설명을 작성하는 용도
+//   형태1 : 라인 주석  //여기서부터 이 줄 끝까지 주석
+//   형태2 : 영역 주석 /* 주석 내용 */
+
+
+
+
