62fdf0a37525b7ce6be3d3d1bad76e36afbf4bd1
shared.repo.z <shared.repo.z@gmail.com>
2021-08-18 17:05:13 +0900

    update code

--- workspaces/java-web/examples-web/src/main/java/com/examplesweb/servlet/HandleRequestServlet.java
+++ workspaces/java-web/examples-web/src/main/java/com/examplesweb/servlet/HandleRequestServlet.java
@@ -15,17 +15,38 @@ public class HandleRequestServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		// get request data encoding : default utf-8
+		
 		String name = req.getParameter("name"); // name="name"인 입력 요소의 데이터 읽기
 		String email = req.getParameter("email"); // name="email"인 입력 요소의 데이터 읽기
 		
-		System.out.printf("[%s][%s]", name, email); // 서버의 콘솔에 출력
+		System.out.printf("GET REQUEST : [%s][%s]\n", name, email); // 서버의 콘솔에 출력
 	
 		// html text 형식으로 응답
 		resp.setContentType("text/html;charset=utf-8"); //브라우저에게 이 응답이 html 형식이며 utf-8 방식으로 작성된 것을 알려주는 설정
 		PrintWriter out = resp.getWriter();
-		String message = String.format("<h2>[%s][%s]</h2>", name, email);
+		String message = String.format("<h2>GET RESPONSE : [%s][%s]</h2>", name, email);
 		out.println(message); // 응답 스트림에 데이터 쓰기 --> 브라우저에게 전달
 	
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// post request data encoding : default ascii -> 한글 데이터를 읽으려면 encoding 수정 필요
+		req.setCharacterEncoding("utf-8"); // 요청 데이터를 읽을 때 사용할 인코딩 지정
+		String name = req.getParameter("name"); // name="name"인 입력 요소의 데이터 읽기
+		String email = req.getParameter("email"); // name="email"인 입력 요소의 데이터 읽기
+		
+		System.out.printf("POST REQUEST : [%s][%s]\n", name, email); // 서버의 콘솔에 출력
+	
+		// html text 형식으로 응답
+		resp.setContentType("text/html;charset=utf-8"); //브라우저에게 이 응답이 html 형식이며 utf-8 방식으로 작성된 것을 알려주는 설정
+		PrintWriter out = resp.getWriter();
+		String message = String.format("<h2>POST RESPONSE : [%s][%s]</h2>", name, email);
+		out.println(message); // 응답 스트림에 데이터 쓰기 --> 브라우저에게 전달
+		
+		
 	}
 
 }
