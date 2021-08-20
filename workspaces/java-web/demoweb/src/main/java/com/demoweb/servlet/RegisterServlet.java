617f1dadde35e1689abe34e4e98e1d6d085cefa5
shared.repo.z <shared.repo.z@gmail.com>
2021-08-20 09:54:38 +0900

    add files

---
+++ workspaces/java-web/demoweb/src/main/java/com/demoweb/servlet/RegisterServlet.java
@@ -0,0 +1,22 @@
+package com.demoweb.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/account/register.action" })
+public class RegisterServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/account/register.jsp");
+		dispatcher.forward(req, resp);
+
+	}
+}
