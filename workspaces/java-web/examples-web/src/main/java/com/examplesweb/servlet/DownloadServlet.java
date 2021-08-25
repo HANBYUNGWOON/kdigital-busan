c31343f84d929debc2de34e6edbe6944727a5a39
shared.repo.z <shared.repo.z@gmail.com>
2021-08-25 10:52:41 +0900

    update code

--- workspaces/java-web/examples-web/src/main/java/com/examplesweb/servlet/DownloadServlet.java
+++ workspaces/java-web/examples-web/src/main/java/com/examplesweb/servlet/DownloadServlet.java
@@ -1,7 +1,11 @@
 package com.examplesweb.servlet;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,6 +26,39 @@ public class DownloadServlet extends HttpServlet {
 			resp.sendRedirect("g.file-list.jsp");
 			return;
 		}
+		
+		//ServletContext : JSP의 application객체와 동일한 객체
+		ServletContext application = req.getServletContext();		
+		String path = application.getRealPath("/upload-files/" + fileName);	// 다운로드할 파일의 전체 경로		
+		
+		System.out.println(fileName);
+		
+		File f = new File(path);
+		if (!f.exists()) { // 다운로드 요청한 파일이 서버에 없는 경우
+			resp.sendRedirect("g.file-list.jsp");
+			return;
+		}
+		
+		//브라우저가 응답 컨텐츠를 다운로드로 처리하도록 정보 설정
+		resp.setContentType("application/octet-stream;charset=utf-8");	
+		
+		//브라우저에게 다운로드하는 파일의 이름을 알려주는 코드 
+		resp.addHeader("Content-Disposition", 
+				"Attachment;filename=\"" + new String(fileName.getBytes("utf-8"), "ISO-8859-1") + "\"");
+		
+		FileInputStream fis = new FileInputStream(path); 	//파일을 읽는 도구
+		OutputStream fos = resp.getOutputStream();			//브라우저에게 전송하는 도구
+		
+		while (true) {
+			int data = fis.read();  //파일에서 1byte 읽기
+			if (data == -1) { //더 이상 읽을 데이터가 없다면 (EOF)
+				break;
+			}
+			fos.write(data); //응답 스트림에 1byte 쓰기
+		}
+		
+		fis.close();
+		fos.close();
 		
 	}
 
