2a303f29ac5154d214df7532f9a69ba6f10409e8
shared.repo.z <shared.repo.z@gmail.com>
2021-09-09 11:36:01 +0900

    update code

--- workspaces/spring-basic/spring-demoweb2/src/main/java/com/demoweb/dao/JdbcTemplateBoardDaoImpl.java
+++ workspaces/spring-basic/spring-demoweb2/src/main/java/com/demoweb/dao/JdbcTemplateBoardDaoImpl.java
@@ -1,9 +1,16 @@
 package com.demoweb.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.demoweb.vo.BoardVO;
+
+import lombok.Setter;
 
 public class JdbcTemplateBoardDaoImpl implements BoardDao {
 
+	@Setter
+	private JdbcTemplate jdbcTemplate;
+	
 	@Override
 	public int insertBoard(BoardVO board) {
 		return 0;
