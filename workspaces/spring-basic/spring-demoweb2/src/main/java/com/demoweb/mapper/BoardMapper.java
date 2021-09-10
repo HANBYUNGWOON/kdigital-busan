44d667f8c3196e1d51c68a26bdb74757990c91b2
shared.repo.z <shared.repo.z@gmail.com>
2021-09-10 16:14:18 +0900

    update code

---
+++ workspaces/spring-basic/spring-demoweb2/src/main/java/com/demoweb/mapper/BoardMapper.java
@@ -0,0 +1,16 @@
+package com.demoweb.mapper;
+
+import java.util.List;
+
+import com.demoweb.vo.BoardVO;
+
+// BoardMapper.xml 파일과 매핑되는 인터페이스 정의
+// -> BoardMapper.xml과 경로와 이름이 일치하도록 작성
+public interface BoardMapper {
+	
+	// 인터페이스메서드는 BoardMapper.xml 파일의 insert, update, select, ... 의 아이디과 일치하도록 작성
+	void insertBoard(BoardVO board);
+	
+	List<BoardVO> selectAll();
+
+}
--- workspaces/spring-basic/spring-demoweb2/src/main/java/com/demoweb/service/BoardServiceImpl.java
+++ workspaces/spring-basic/spring-demoweb2/src/main/java/com/demoweb/service/BoardServiceImpl.java
@@ -3,6 +3,7 @@ package com.demoweb.service;
 import java.util.List;
 
 import com.demoweb.dao.BoardDao;
+import com.demoweb.mapper.BoardMapper;
 import com.demoweb.vo.BoardVO;
 
 import lombok.Setter;
@@ -12,10 +13,14 @@ public class BoardServiceImpl implements BoardService {
 	@Setter // setBoardDao를 자동으로 만드는 annotation
 	private BoardDao boardDao;
 	
+	@Setter
+	private BoardMapper boardMapper;
+	
 	@Override
 	public int writeBoard(BoardVO board) {
 		
-		boardDao.insertBoard(board);
+		//boardDao.insertBoard(board);
+		boardMapper.insertBoard(board);
 		
 		return 0;
 	}
@@ -23,7 +28,9 @@ public class BoardServiceImpl implements BoardService {
 	@Override
 	public List<BoardVO> findAll() {
 
-		List<BoardVO> boards = boardDao.selectAll();
+		//List<BoardVO> boards = boardDao.selectAll();
+		List<BoardVO> boards = boardMapper.selectAll();
+		
 		return boards;
 	}
 
